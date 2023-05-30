9657e2dad1a01ca9abe74278121eff104c82875b
Freddy Jiang <jtf.imperial@gmail.com>
2023-05-30 20:23:32 +0100

    Update lerp speed

--- view/const.py
+++ view/const.py
@@ -1,7 +1,7 @@
 PROD_DEFAULT = False
 FPS = 60
 FRAMES_PER_ENTRY_DEFAULT = 2.5
-RANK_LERP_KERNEL_SIZE = 11
+RANK_LERP_KERNEL_SIZE = 3
 
 # Local screen is too small for 1920x1088
 # So if we are not in production mode, dimensions are halved
